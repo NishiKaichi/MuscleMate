e93ae9421a41c3b7e6ac5ab7e9730604ded34422
Kurage-1223 <yu.sai.moko@gmail.com>
2024-08-04 23:35:39 +0900

    How-to-flask_1st

---
+++ Kurage_how_to_flask/2_receive-data/app_2.py
@@ -0,0 +1,29 @@
+#初期化
+import flask
+app_2 = flask.Flask(__name__)
+
+#入力を促すページの定義
+#index関数が返却する文字列は、HTML文になっています。 内容はとてもシンプルで、入力を受け取るためのformが一つと、その中に入力欄と送信ボタンが一つずつあるだけのものです。
+@app_2.route('/')
+def index():
+    return '''
+    <form action="/receive">
+        <input name="value">
+        <input type="submit">
+    </form>
+    '''
+
+#入力を受け取るページの定義
+@app_2.route('/receive')
+def receive():
+    name = flask.request.args.get('value')
+    if name == 'admin':
+        return 'this is admin page'
+    else:
+        return 'welcome ' + name
+
+
+
+#Webサーバの実行
+if __name__ == '__main__':   #必須の記述
+    app_2.run()                #引き金
