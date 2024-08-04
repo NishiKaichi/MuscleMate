9307b48435ca765e4e9d811384b2d2f04c11a527
Kurage-1223 <yu.sai.moko@gmail.com>
2024-08-05 01:26:20 +0900

    ChatGPT_sns 1st

---
+++ ChatGPT_sns_project/app/routes.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+from .forms import RegistrationForm, LoginForm, PostForm
+from .models import db, User, Post
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created successfully!', 'success')
+        return redirect(url_for('main.index'))
+    return render_template('register.html', form=form)
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.password == form.password.data:
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('main.index'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', form=form)
+
+@main.route('/post/new', methods=['GET', 'POST'])
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=User.query.first()) # Replace with actual user logic
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('main.index'))
+    return render_template('post.html', form=form)
