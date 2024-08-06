7788743117cdf7a0ff5edbc06e22e0b4072665f7
Kurage-1223 <yu.sai.moko@gmail.com>
2024-08-06 17:52:50 +0900

    ログイン画面とユーザー登録画面の編集、それに伴ったlayout,layout_loginの修正。ファイルの整理

--- haikusns/login_form.py
+++ haikusns/login_form.py
@@ -4,14 +4,8 @@ import os, time
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/login')
 def login():
     return render_template('login_form.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
