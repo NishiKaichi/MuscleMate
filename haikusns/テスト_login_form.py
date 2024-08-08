56265bf97c2ac674eda930510a6fe5b597cb299a
Kurage_1223 <yu.sai.moko@gmail.com>
2024-08-09 00:32:18 +0900

    前回のコミットと同期後、にapp.pyを実行した際ログイン画面とユーザー登録画面が崩れているのを確認。login_form.htmlとregister.htmlそれぞれのテスト用実行ファイルを作成し 実行したところ同様の崩れを確認したが寝るのでそのままだよごめんね。心当たりはあるからあとで直します。（多分style.cssでページの表示範囲やコードの形式いじったときに逝った）

---
+++ haikusns/テスト_login_form.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect, render_template, request
+from markupsafe import Markup
+import os, time
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login_form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
