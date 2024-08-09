768f55a700b3d579828858f0ad9f0b41cd59f830
shinjio2170 <shinjio494@gmail.com>
2024-08-09 15:36:58 +0900

    Add files from existing repository

--- hello.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask             #Flaskクラスのインポート
-
-app=Flask(__name__)                 #Flaskクラスのインスタンスを作成
-
-@app.route("/")                     #routeデコレータを使ってどのURLが関数の引き金になるのかを伝える
-def hello_world():                  
-    return "<p>tsttstststsaaaaaatststsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>"
--- helloworld.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = (__name__)
-
-@app.route("/")
-def hello():
-    return "こんにちはこんばんはぎこちゃんです"
